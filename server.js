de5980b8fa23f82650eb517466d9ba1af8e0d43d
Nur Muh. Briva Rizky O M <114548981+Brivariky@users.noreply.github.com>
2025-10-18 00:13:54 +0700

    Update CORS origin for middleware

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ async function importSampleData() {
 
 // Middleware
 app.use(cors({
-  origin: 'https://labinti.vercel.app',
+  origin: 'https://lab-inventaris-fe-test.vercel.app/',
   credentials: true
 }));
 
